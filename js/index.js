8ed99056e3a34bd5d5a6bbfaf386cdc7ff55131e
Vladyslav <ryzhykovvv@gmail.com>
2022-05-03 14:52:42 +0300

    connected burger menu

--- js/index.js
+++ js/index.js
@@ -1,47 +1,68 @@
-const presentOrderBtn = document.querySelector('.present__order-btn');
-const pageOverlayModal = document.querySelector('.page__overlay_modal');
-const modalClose = document.querySelector('.modal__close-svg');
+{
+  const presentOrderBtn = document.querySelector('.present__order-btn');
+  const pageOverlayModal = document.querySelector('.page__overlay_modal');
+  const modalClose = document.querySelector('.modal__close-svg');
 
 
-let handlerModal = (btnOpen, modal, changeClass, btnClose, openingSpeed) => {
-// можно не исп тернарн опер - присв занчен по умолч openingSpeed = 'default') =>
+  let handlerModal = (btnOpen, modal, changeClass, btnClose, openingSpeed) => {
+  // можно не исп тернарн опер - присв занчен по умолч openingSpeed = 'default') =>
 
-  let opacity = 0;
+    let opacity = 0;
 
-  const speedTypes = {
-    slow: 15,
-    medium: 8,
-    fast: 1,
-    default: 5,
+    const speedTypes = {
+      slow: 15,
+      medium: 8,
+      fast: 1,
+      default: 5,
+    };
+
+    btnOpen.addEventListener('click', () => {
+      modal.style.opacity = opacity;
+
+      modal.classList.add(changeClass);
+
+      let changeOpacity = setInterval(() => {
+        opacity = opacity + 0.02;
+        modal.style.opacity = opacity;
+
+        if (opacity >= 1) clearInterval(changeOpacity);
+      }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
+    });
+
+    btnClose.addEventListener('click', () => {
+
+      let changeOpacity = setInterval(() => {
+        opacity = opacity - 0.02;
+        modal.style.opacity = opacity;
+
+        if (opacity <= 0) {
+          clearInterval(changeOpacity);
+          modal.classList.remove(changeClass);
+        }
+      }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
+    });
+
   };
 
-  btnOpen.addEventListener('click', () => {
-    modal.style.opacity = opacity;
+  handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'slow');
+}
 
-    modal.classList.add(changeClass);
+{
+  const headerContactsBurger = document.querySelector('.header__contacts-burger');
+  const headerContacts = document.querySelector('.header__contacts');
+  
+  let handlerBurger = (btnClick, menu, changeClass) => {
+    btnClick.addEventListener('click', () => {
 
-    let changeOpacity = setInterval(() => {
-      opacity = opacity + 0.02;
-      modal.style.opacity = opacity;
-
-      if (opacity >= 1) clearInterval(changeOpacity);
-    }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
-  });
-
-  btnClose.addEventListener('click', () => {
-
-    let changeOpacity = setInterval(() => {
-      opacity = opacity - 0.02;
-      modal.style.opacity = opacity;
-
-      if (opacity <= 0) {
-        clearInterval(changeOpacity);
-        modal.classList.remove(changeClass);
+      if (menu.classList.contains(changeClass)) {
+        menu.style.height = '';
+        menu.classList.remove(changeClass);
+      } else {
+        menu.style.height = menu.scrollHeight + 'px'; // измерение высоты контента в элементе, включая содержимое
+        menu.classList.add(changeClass);  // высота меню будет подстраиваться под наполнения контентом
       }
-    }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
-  });
+    });
+  };
 
-};
-
-handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'slow');
-
+  handlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open');
+}
