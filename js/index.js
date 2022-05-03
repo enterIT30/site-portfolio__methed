26a95aabd44111e33a67b8cfc7fd10c06f13d7b3
Vladyslav <ryzhykovvv@gmail.com>
2022-05-03 22:01:02 +0300

    fix modal

--- js/index.js
+++ js/index.js
@@ -1,51 +1,99 @@
-{
-  const presentOrderBtn = document.querySelector('.present__order-btn');
-  const pageOverlayModal = document.querySelector('.page__overlay_modal');
-  const modalClose = document.querySelector('.modal__close-svg');
+const presentOrderBtn = document.querySelector('.present__order-btn');
+const pageOverlayModal = document.querySelector('.page__overlay_modal');
+const modalClose = document.querySelector('.modal__close-svg');
 
 
-  let handlerModal = (btnOpen, modal, changeClass, btnClose, openingSpeed) => {
-  // можно не исп тернарн опер - присв занчен по умолч openingSpeed = 'default') =>
 
-    let opacity = 0;
+let handlerModal = (btnOpen, modal, changeClass, btnClose, openingSpeed) => {
+// можно не исп тернарн опер - присв занчен по умолч openingSpeed = 'default') =>
 
-    const speedTypes = {
-      slow: 15,
-      medium: 8,
-      fast: 1,
-      default: 5,
-    };
+  let opacity = 0;
 
-    btnOpen.addEventListener('click', () => {
-      modal.style.opacity = opacity;
-
-      modal.classList.add(changeClass);
-
-      let changeOpacity = setInterval(() => {
-        opacity = opacity + 0.02;
-        modal.style.opacity = opacity;
-
-        if (opacity >= 1) clearInterval(changeOpacity);
-      }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
-    });
-
-    btnClose.addEventListener('click', () => {
-
-      let changeOpacity = setInterval(() => {
-        opacity = opacity - 0.02;
-        modal.style.opacity = opacity;
-
-        if (opacity <= 0) {
-          clearInterval(changeOpacity);
-          modal.classList.remove(changeClass);
-        }
-      }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
-    });
-
+  const speedTypes = {
+    slow: 15,
+    medium: 8,
+    fast: 1,
+    default: 5,
   };
 
-  handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'slow');
+  function openModal() {
+    disabledScroll();
+    modal.style.opacity = opacity;
+
+    modal.classList.add(changeClass);
+
+    let changeOpacity = setInterval(() => {
+      opacity = opacity + 0.02;
+      modal.style.opacity = opacity;
+
+      if (opacity >= 1) clearInterval(changeOpacity);
+    }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
+  }
+
+  function closeModal(){
+    enabledScroll();
+    let changeOpacity = setInterval(() => {
+      opacity = opacity - 0.02;
+      modal.style.opacity = opacity;
+
+      if (opacity <= 0) {
+        clearInterval(changeOpacity);
+        modal.classList.remove(changeClass);
+      }
+    }, speedTypes[openingSpeed] ? speedTypes[openingSpeed] : speedTypes.default);
+  }
+
+  btnOpen.addEventListener('click', openModal); // можно опустить стрелочную функцию и скобки вызова
+
+  btnClose.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => { // закрытие при клике на пустоту
+    console.log(e.target);
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
 }
+
+/* плохо работает на iPhone
+  function disabledScroll() {
+  document.body.style.overflow = 'hidden';
+} */
+
+// более надужный вариант
+function disabledScroll() {
+
+  // const widthScroll = window.innerHeight - document.body.offsetWidth;
+  /* нужно добавить, если нужно убрать скачек за счет появления скрола
+  padding-right: ${widthScroll} */
+  
+
+  document.body.scrollPosition = window.scrollY;// создание св-ва эл-та body
+
+  document.body.style.cssText =`
+  overflow: hidden;
+  position: fixed;
+  top: -${document.body.scrollPosition}px; //!!!!!
+  left: 0;
+  height: 100vh;
+  `;
+}
+
+function enabledScroll() {
+  document.body.style.cssText = 'position: relative';
+  window.scroll({top: document.body.scrollPosition}); //!!!!!
+}
+
+handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'slow');
+
+
+
+
+
+
+
+
+
 
 {
   const headerContactsBurger = document.querySelector('.header__contacts-burger');
